c251f063504f021b0f63b802bf36638034de90a7
Your Name <you@example.com>
2023-09-08 11:11:00 -0500

    cambios

--- src/server.js
+++ src/server.js
@@ -2,13 +2,12 @@
 const express = require('express')
 
 // COMPONENTES DE RUTA
-//const port = process.env.PORT || 4000;
-//const app = require ('./App');
+const port = process.env.PORT || 4000;
+const app = require ('./App');
 
 const init = express();
 
 // RUTAS
 init.use(app);
 
-
-//app.listen(port, () => console.log('Example app listening on port', port));
+app.listen(port, () => console.log('Example app listening on port', port));
